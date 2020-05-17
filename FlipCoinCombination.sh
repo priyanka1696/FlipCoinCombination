ae51456ee38f25ed235e038fd071b7352e3d1e05
Priyanka <fulmogarepriya@gmail.com>
2020-05-17 12:49:48 +0530

    Flipping Double Coin And Determine Percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -45,3 +45,42 @@ then
 else
 	echo "Winner Is Head"
 fi
+
+declare -A DoubletDict
+DoubletDict+=(["HH"]=0 ["HT"]=0 ["TT"]=0 ["TH"]=0)
+for ((i = 0; i <$n; i++)); 
+do
+randomCheck=$((RANDOM%4))
+	case $check in
+    		0)
+    	  	  DoubletDict["HH"]=$((${DoubletDict["HH"]}+1))
+    	  	  ;;
+    		1)
+	  	  DoubletDict["HT"]=$((${DoubletDict["HT"]}+1))
+    	  	  ;;
+    		2)
+    	   	  DoubletDict["TT"]=$((${DoubletDict["TT"]}+1))
+          	  ;;
+    		3)
+    	  	  DoubletDict["TH"]=$((${DoubletDict["TH"]}+1))
+    	  	  ;;
+	esac
+done
+
+for i in ${!DoubletDict[@]}
+do
+    DoubletDict[$i]=$(((${DoubletDict[$i]}*100)/$n))
+done
+arrayDoublet=($(for value in ${DoubletDict[@]}
+do
+    echo $value
+done | sort ))
+max=${arrayDoublet[3]}
+echo ${arrayDoublet[@]}
+for key in ${!DoubletDict[@]}
+do
+    if [[ $max -eq ${DoubletDict[$key]} ]]
+    then
+    echo "win combo $key"
+    fi
+done
