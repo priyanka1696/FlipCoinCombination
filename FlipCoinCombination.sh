09fe41f43ce27473d7ec669670a8b8d3ed650e55
Priyanka <fulmogarepriya@gmail.com>
2020-05-17 13:08:05 +0530

    Flipping Triple Coin Determine Percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -51,7 +51,7 @@ DoubletDict+=(["HH"]=0 ["HT"]=0 ["TT"]=0 ["TH"]=0)
 for ((i = 0; i <$n; i++)); 
 do
 randomCheck=$((RANDOM%4))
-	case $check in
+	case $randomcheck in
     		0)
     	  	  DoubletDict["HH"]=$((${DoubletDict["HH"]}+1))
     	  	  ;;
@@ -80,6 +80,57 @@ echo ${arrayDoublet[@]}
 for key in ${!DoubletDict[@]}
 do
     if [[ $max -eq ${DoubletDict[$key]} ]]
+    then
+    echo "win combo $key"
+    fi
+done
+
+declare -A TripletDict
+TripletDict+=(["HHH"]=0 ["HHT"]=0 ["HTH"]=0 ["THH"]=0  ["TTT"]=0 ["TTH"]=0 ["THT"]=0 ["HTT"]=0)
+for ((i = 0; i <$n ; i++)); 
+do
+randomCheck=$((RANDOM%8))
+	case $randomCheck in
+    		0)
+    		  TripletDict["HHH"]=$((${TripletDict["HHH"]}+1))
+    		  ;;
+    		1)
+    		  TripletDict["HHT"]=$((${TripletDict["HHT"]}+1))
+    		  ;;
+    		2)
+    		  TripletDict["THH"]=$((${TripletDict["HTH"]}+1))
+    		  ;;
+    		3)
+   		  TripletDict["THH"]=$((${TripletDict["THH"]}+1))
+    		  ;;
+    		4)
+    		  TripletDict["TTT"]=$((${TripletDict["TTT"]}+1))
+    		  ;;
+    		5)
+    		  TripletDict["TTH"]=$((${TripletDict["TTH"]}+1))
+    		  ;;
+    		6)
+    		  TripletDict["THT"]=$((${TripletDict["THT"]}+1))
+    		  ;;
+    		7)
+    		  TripletDict["HTT"]=$((${TripletDict["HTT"]}+1))
+    		  ;;
+	esac
+done
+
+for i in ${!TripletDict[@]}
+do
+    TripletDict[$i]=$(((${TripletDict[$i]}*100)/$n))
+done
+arrayTriplet=($(for value in ${TripletDict[@]}
+do
+    echo $value
+done | sort ))
+max=${arrayTriplet[7]}
+echo ${arrayTriplet[@]}
+for key in ${!TripletDict[@]}
+do
+    if [ $max -eq ${TripletDict[$key]} ]
     then
     echo "win combo $key"
     fi
