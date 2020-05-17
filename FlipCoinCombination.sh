d40380d52c367893a720bf32b2874d1fd9a2693d
Priyanka <fulmogarepriya@gmail.com>
2020-05-17 12:35:15 +0530

    Flipping Coin Determine The Percentage Of Singlet

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -11,3 +11,37 @@ else
 	echo "Tail"
 fi
 
+echo "Enter Number"
+read n
+tails=0;
+heads=0;
+for ((i=0;i<$n;i++))
+do
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		tails=`expr $tails + 1`
+	else
+		heads=`expr $heads + 1`
+	fi
+done
+headper=$((100*$heads/$n))
+echo "Head Percentage ="$headper"%"
+tailper=$((100*$tails/$n))
+echo "Tail Percentage ="$tailper"%"
+
+declare -A SingletDict
+SingleDict["Heads"]=$headper
+SingletDict["Tails"]=$tailper
+
+if [[ ${SingletDict[headper]} -gt ${SingletDict[tailper]} ]]
+then
+	SingletDict[headper]=${SingletDict[tailper]}
+	SingletDict[tailper]=$temp
+fi
+	echo ${SingletDict[@]}
+if [[ $tails -gt $heads ]]
+then
+	echo "Winner Is Tail"
+else
+	echo "Winner Is Head"
+fi
